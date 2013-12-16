c345f1e7863c0483b675ac8a475f26baf5767a29
sulphuric <h2so4_nex@outlook.com>
2013-12-16 17:38:07 +0530

    fixed minor bug and organized code

--- speak.php
+++ speak.php
@@ -6,8 +6,8 @@ header('Content-type: text/plain');
 if(empty($_GET ["say"]) || empty($_GET ["type"]))
 	return;
 
-$say = $_GET["say"];
 
+$say = $_GET["say"];
 
 $possible_null_char = substr($say, 0,6);
 if(strcmp($possible_null_char, "--NULL") == 0)
@@ -16,28 +16,27 @@ if(strcmp($possible_null_char, "--NULL") == 0)
 	return;
 }
 
-
-
-
 $type = $_GET["type"];
 
-
-$length = strlen($say);
 
 if($type == 1)
 	print "say ";
 elseif($type == 2)
 	print "say_team ";
 $color_count = 1;
+$color_type = 1;
 
 $char = "A";//some garbage character other than white space
 $trimmed_char = "";
-//print "\"";
+
 $space = 0;
 $trimmed_char = TrimAlreadyExistingColorCodes($say);
 
-
+#Debug
+#print "--- $trimmed_char--- ";
+#debug
 $prev_char = "x";//some garbage value other than $
+$length = strlen($trimmed_char);
 for($i = 0; $i <$length; ++$i)
 {
 	$char = substr($trimmed_char, $i,1);
@@ -46,78 +45,8 @@ for($i = 0; $i <$length; ++$i)
 		$space = 1;
 	}
 	
-	
 	if($space == 0) {
-	//Add color
-	switch ($color_count){
-		case 1 :
-			print "^x700";
-			++$color_count;
-			break;
-		case 2 :
-			print "^xA10";
-			++$color_count;
-			break;
-		case 3 :
-			print "^xE20";
-			++$color_count;
-			break;
-		case 4 :
-			print "^xF21";
-			++$color_count;
-			break;
-		case 5  :
-			print "^xF33";
-			++$color_count;
-			break;
-		case 6 :
-			print "^xF76";
-			++$color_count;
-			break;
-		case 7 :
-			print "^xF77";
-			++$color_count;
-			break;
-		case 8 :
-			print "^xFAA";
-			++$color_count;
-			break;
-		case 9 :
-			print "^xFEE";
-			++$color_count;
-			break;
-		#reverse color
-		case 10 :#same as 8
-			print "^xFAA";
-			++$color_count;
-			break;
-		case 11 :#same as 7
-			print "^xF77";
-			++$color_count;
-			break;
-		case 12 :#same as 6
-			print "^xF76";
-			++$color_count;
-			break;
-		case 13 :#same as 5
-			print "^xF33";
-			++$color_count;
-			break;
-		case 14 :#same as 4
-			print "^xF21";
-			++$color_count;
-			break;
-		case 15 :#same as 3
-			print "^xE20";
-			++$color_count;
-			break;
-		case 16 :#same as 2
-			print "^xA10";
-			$color_count = 1;
-			break;
-		default:
-			break;
-		}
+		$color_count = colorify($color_count, $color_type);
 	}
 	else
 	{
@@ -133,7 +62,7 @@ for($i = 0; $i <$length; ++$i)
 	if(strcmp($prev_char , "\$") == 0)
 	{
 	
-		if( strcmp($char , "{") == 0)
+		if(strcmp($char , "{") == 0)
 		{
 			break;
 		}
@@ -144,6 +73,173 @@ for($i = 0; $i <$length; ++$i)
 		}
 	}
 
+	PrintSpecial($char);
+	
+}
+
+function TrimAlreadyExistingColorCodes($say){
+	$trimmed_char = " ";
+	$color_code_position = 0;
+	$stringlength = strlen($say);
+	$position_B = $stringlength;  # A........B..A............B
+	$position_A = 0;  # A........^1.A............^xCF0.A......
+	$position_B_memory = $position_B; #need to reset if color code verification fails
+	for($j = 0; $j <$stringlength; ++$j)
+	{
+		$temp_char = substr($say, $j,1);
+		
+		if(strcmp ($temp_char, "^") == 0)
+		{
+			$color_code_position  = 1 ;
+		}
+		elseif($color_code_position == 1 )
+		{
+		
+			if($temp_char <= 9 && $temp_char >=0 && is_numeric($temp_char))
+			{
+				#add1;
+				$position_B = $j - 1 ;
+				
+				if($position_B - $position_A > 0)
+				{
+					$trimmed_char = $trimmed_char.substr($say, $position_A, $position_B - $position_A);
+				}
+			
+				$position_A = $position_B + 2;
+				$position_B = $stringlength;
+				$color_code_position = 0;
+				
+			}
+			elseif (strcmp ( $temp_char , "x")==0)
+			{
+				$color_code_position  = 2;
+			}
+			else
+			{
+				$color_code_position = 0;
+			#	$position_B = $position_B_memory; # color code verification failed; something like ...^a..
+			}
+			
+		}
+		elseif($color_code_position == 2 || $color_code_position == 3 || $color_code_position == 4 || $color_code_position == 5)
+		{
+		
+			if( (0 <= $temp_char && $temp_char <= 9 && is_numeric($temp_char)) || strcmp ( $temp_char , "A" ) == 0||
+			strcmp ( $temp_char , "B") == 0 || strcmp( $temp_char, "C") == 0|| strcmp ( $temp_char , "D") == 0|| strcmp ( $temp_char , "E") == 0|| strcmp ( $temp_char , "F") == 0
+			|| strcmp ( $temp_char , "a") == 0 || strcmp ( $temp_char , "b") == 0 || strcmp ( $temp_char , "c") == 0|| strcmp ( $temp_char , "d"|| strcmp ( $temp_char , "e") == 0|| strcmp ( $temp_char , "f") == 0))
+			{
+				++$color_code_position;
+			}
+			else
+			{
+				$color_code_position = 0;
+				#???
+			}
+			
+			if($color_code_position == 4)
+			{
+				#add2;
+				
+				$position_B = $j - 3;
+				if($position_B - $position_A > 0)
+				{
+					$trimmed_char = $trimmed_char.substr($say, $position_A, $position_B- $position_A );
+				}
+			
+				$position_A = $position_B + 5;
+				$position_B =$stringlength;
+				$color_code_position = 0;
+			}
+				
+		}
+	}
+	
+	$trimmed_char = $trimmed_char.substr($say, $position_A, $position_B - $position_A);
+	return $trimmed_char;
+}
+
+function colorify($color_count, $color_type)
+{
+		switch ($color_count){
+			case 1 :
+				switch($color_type){
+					case 1:
+						print "^x700";
+						++$color_count;
+						break;
+					default:
+						break;
+				}
+					break;
+			case 2 :
+				print "^xA10";
+				++$color_count;
+				break;
+			case 3 :
+				print "^xE20";
+				++$color_count;
+				break;
+			case 4 :
+				print "^xF21";
+				++$color_count;
+				break;
+			case 5  :
+				print "^xF33";
+				++$color_count;
+				break;
+			case 6 :
+				print "^xF76";
+				++$color_count;
+				break;
+			case 7 :
+				print "^xF77";
+				++$color_count;
+				break;
+			case 8 :
+				print "^xFAA";
+				++$color_count;
+				break;
+			case 9 :
+				print "^xFEE";
+				++$color_count;
+				break;
+			#reverse color
+			case 10 :#same as 8
+				print "^xFAA";
+				++$color_count;
+				break;
+			case 11 :#same as 7
+				print "^xF77";
+				++$color_count;
+				break;
+			case 12 :#same as 6
+				print "^xF76";
+				++$color_count;
+				break;
+			case 13 :#same as 5
+				print "^xF33";
+				++$color_count;
+				break;
+			case 14 :#same as 4
+				print "^xF21";
+				++$color_count;
+				break;
+			case 15 :#same as 3
+				print "^xE20";
+				++$color_count;
+				break;
+			case 16 :#same as 2
+				print "^xA10";
+				$color_count = 1;
+				break;
+			default:
+				break;
+		}
+		return $color_count;
+}
+
+function PrintSpecial($char)
+{
 	switch ($char) {
 		case "A":
 			print "á";
@@ -257,89 +353,5 @@ for($i = 0; $i <$length; ++$i)
 			print "$char";
 			break;
 	}
-	
 }
-
-
-function TrimAlreadyExistingColorCodes($say){
-	$trimmed_char = " ";
-	$color_code_position = 0;
-	$stringlength = strlen($say);
-	$position_B = $stringlength;  # A........B..A............B
-	$position_A = 0;  # A........^1.A............^xCF0.A......
-	$position_B_memory = $position_B; #need to reset if color code verification fails
-	for($j = 0; $j <$stringlength; ++$j)
-	{
-		$temp_char = substr($say, $j,1);
-		
-		if(strcmp ($temp_char, "^") == 0)
-		{
-			$color_code_position  = 1 ;
-		}
-		elseif($color_code_position == 1 )
-		{
-		
-			if($temp_char <= 9 && $temp_char >=0 && is_numeric($temp_char))
-			{
-				#add1;
-				$position_B = $j - 1 ;
-				
-				if($position_B - $position_A > 0)
-				{
-					$trimmed_char = $trimmed_char.substr($say, $position_A, $position_B - $position_A);
-				}
-			
-				$position_A = $position_B + 2;
-				$position_B = $stringlength;
-				$color_code_position = 0;
-				
-			}
-			elseif (strcmp ( $temp_char , "x")==0)
-			{
-				$color_code_position  = 2;
-			}
-			else
-			{
-				$color_code_position = 0;
-			#	$position_B = $position_B_memory; # color code verification failed; something like ...^a..
-			}
-			
-		}
-		elseif($color_code_position == 2 || $color_code_position == 3 || $color_code_position == 4 || $color_code_position == 5)
-		{
-		
-			if( (0 <= $temp_char && $temp_char <= 9 && is_numeric($temp_char)) || strcmp ( $temp_char , "A" ) == 0||
-			strcmp ( $temp_char , "B") == 0 || strcmp( $temp_char, "C") == 0|| strcmp ( $temp_char , "D") == 0|| strcmp ( $temp_char , "E") == 0|| strcmp ( $temp_char , "F") == 0
-			|| strcmp ( $temp_char , "a") == 0 || strcmp ( $temp_char , "b") == 0 || strcmp ( $temp_char , "c") == 0|| strcmp ( $temp_char , "d"|| strcmp ( $temp_char , "e") == 0|| strcmp ( $temp_char , "f") == 0))
-			{
-				++$color_code_position;
-			}
-			else
-			{
-				$color_code_position = 0;
-				#???
-			}
-			
-			if($color_code_position == 4)
-			{
-				#add2;
-				
-				$position_B = $j - 3;
-				if($position_B - $position_A > 0)
-				{
-					$trimmed_char = $trimmed_char.substr($say, $position_A, $position_B- $position_A );
-				}
-			
-				$position_A = $position_B + 5;
-				$position_B =$stringlength;
-				$color_code_position = 0;
-			}
-				
-		}
-	}
-	
-	$trimmed_char = $trimmed_char.substr($say, $position_A, $position_B - $position_A);
-	return $trimmed_char;
-}
-
 ?>
