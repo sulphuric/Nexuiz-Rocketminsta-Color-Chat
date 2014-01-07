14a35ed1fa6ddbe6aab71f6b0ebe47effcfc4a82
sulphuric <h2so4_nex@outlook.com>
2014-01-07 12:08:57 +0530

    batch file to call nexuiz added

---
+++ nexuiz-sdl.bat
@@ -0,0 +1,7 @@
+:: Place this in Nexuiz main folder
+:: Set your xampp path
+:: Then you can click this file to open nexuiz
+YourServerpath\xampp\xampp_start.exe
+del .\data\dlcache\*.chat
+nexuiz-sdl.exe
+YourServerPath\xampp\xampp_stop.exe
